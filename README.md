dd9ead954010f8df68e92168297ab03f77710e97
Simon Rdding <s1m0n.r3dd1ng@gmail.com>
2016-11-11 23:00:47 +0000

    compojure project dependencies added

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # rydz
+
+Private Hire booking interfaces
+
+## Installation
+
+
+Download from http://github.com/OldIronHorse/rydz
+
+## Usage
+
+FIXME: explanation
+
+    $ java -jar rydz-0.1.0-standalone.jar [args]
+
+## Options
+
+FIXME: listing of options this app accepts.
+
+## Examples
+
+...
+
+### Bugs
+
+...
+
+### Any Other Sections
+### That You Think
+### Might be Useful
+
+## License
+
+Copyright © 2016 Simon Redding
+
+Distributed under the GNU Public License verson 3
--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :license {:name "GPLv3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-core "1.4.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-core "1.5.0"]
+                 [ring/ring-jetty-adapter "1.5.0"]
                  [ring/ring-json "0.4.0"]
-                 [ring/ring-defaults "0.2.0"]
+                 [ring/ring-defaults "0.2.1"]
                  [ring.middleware.logger "0.5.0"]
-                 [org.clojure/tools.logging "0.2.4"]
-                 [compojure "1.5.0"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [compojure "1.5.1"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler rydz.rest/app
          :nrepl {:start? true
-                 :port 999*}}
+                 :port 9998}}
   :profiles
     {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                           [org.clojure/data.json "0.2.6"]
